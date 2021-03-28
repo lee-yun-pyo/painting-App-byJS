34645bff47942cf7c40b46888472af3a1368e420
lee-yun-pyo <41375597+lee-yun-pyo@users.noreply.github.com>
2021-03-28 23:33:19 +0900

    add canvas event
    #2.0 ~ 2.2
    canvas, 2d context

---
+++ app.js
@@ -0,0 +1,43 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+// setting canvas sizes (pixel Size - not CSS)
+canvas.width = "450";
+canvas.height = "450"; 
+
+ctx.strokeStyle ="#2c2c2c";
+ctx.lineWidth ="2.5";
+
+let painting = false;
+
+function stopPainting() {
+    painting = false;
+}
+
+function startPainting(event) {
+    painting = true;
+}
+
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if(!painting) {
+        /* not Painting
+        no click move mouse 
+        just creating path and moving path*/
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        /* exctute throughout moving mouse 
+        creating line */
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+if(canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+}
